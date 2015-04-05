6d908a7c33ba14d3f05f43dda6ecd40c9125c48b
AtillaBosma <atil_duty@hotmail.com>
2015-04-05 22:10:17 +0200

    asdf

--- scripts/testscript.js
+++ scripts/testscript.js
@@ -4,6 +4,8 @@ var text = "";
 var i = setInterval(passiveGoldGain, 1000);
 var j =setInterval(updateTexts, 500);
 
+document.getElementById("demo").onclick = function() {upgradeGoldGain()};
+
 function passiveGoldGain(){
   console.log(gold);
   console.log(goldGain)
@@ -19,4 +21,5 @@ function upgradeGoldGain(){
 function updateTexts(){
   document.getElementById("goldGainText").innerHTML = "+ " + goldGain;
   document.getElementById("goldText").innerHTML = text;
+  document.getElementById("upgradeGoldGain").innerHTML = "Upgrade Gold Gain";
 }
