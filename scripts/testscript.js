35d7d5b7785d9071b623c7d4d60f1853d5a87980
AtillaBosma <atil_duty@hotmail.com>
2015-04-07 16:08:53 +0200

    asdf

--- scripts/testscript.js
+++ scripts/testscript.js
@@ -17,7 +17,7 @@ document.getElementById("upgradeGoldGain").style.left = "80px";
 
 document.getElementById("minersText").style.position = "absolute";
 document.getElementById("minersText").style.left = "10px";
-document.getElementById("minersText").style.top = "30px";
+document.getElementById("minersText").style.top = "28px";
 
 document.getElementById("upgradeGoldGainCostText").style.visibility = "hidden";
 
