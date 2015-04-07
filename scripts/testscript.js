d994a08d7c6d5d22bb68faf8f48f2b2b78abe669
AtillaBosma <atil_duty@hotmail.com>
2015-04-07 10:31:07 +0200

    asdf

--- scripts/testscript.js
+++ scripts/testscript.js
@@ -28,11 +28,11 @@ function passiveGoldGain(){
 }
 
 function upgradeGoldGain(){
-  minersAmount += 1;
   if(gold - upgradeGoldGainCost < 0){
     alert("Not enough gold");
   }else if(gold - upgradeGoldGainCost >= 0){
     gold -= upgradeGoldGainCost;
+    minersAmount += 1;
   }
 }
 
