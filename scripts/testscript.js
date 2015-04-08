84c6e2fa45072d59f8912bcd6fe4a17253849bcf
AtillaBosma <atil_duty@hotmail.com>
2015-04-08 09:42:05 +0200

    asdf

--- scripts/testscript.js
+++ scripts/testscript.js
@@ -101,8 +101,9 @@ function upgradeGoldGain(){
 }
 
 function checkGoldCostUpgrades(){
-  document.getElementById("upgradeGoldGain").onmouseover = function(){document.getElementById("upgradeGoldGainCostText").style.visibility = "visible"}
+  document.getElementById("upgradeGoldGain").onmouseover = function(){document.getElementById("upgradeGoldGainCostText").style.visibility = "visible";} 
   text5 = "Cost: " + upgradeGoldGainCostText;
+}
 
 function buildFarmFunction(){
   farms += 1;
