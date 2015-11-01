affa384625879ec3e54e6f2c4d0beae262ea68a9
AtillaBosma <atillabosma@hotmail.com>
2015-11-01 23:26:15 +0100

    DUDUDUD
    PETTERTALE

--- pettertale/scripts/pettertale.js
+++ pettertale/scripts/pettertale.js
@@ -1,6 +1,20 @@
-document.getElementById("githubButton").style.width="619px";
-document.getElementById("githubButton").style.height="245px";
-document.getElementById("githubButton").style.bottom="100px";
-document.getElementById("githubButton").style.right="100px";
-document.getElementById("githubButton").style.position="fixed";
-document.getElementById("githubButton").style.content= "url(pics/battlebackground.png)"
+document.getElementById("dog").style.position="absolute";
+document.getElementById("dog").style.left="36.3%"
+document.getElementById("dog").style.top="7%"
+document.getElementById("dog").style.zIndex="2"
+document.getElementById("dog").style.content= "url(pics/lesserdog.png)"
+
+document.getElementById("greenGrid").style.width="85%";
+document.getElementById("greenGrid").style.height="50%";
+document.getElementById("greenGrid").style.position="absolute";
+document.getElementById("greenGrid").style.left="7%"
+document.getElementById("greenGrid").style.zIndex="0"
+document.getElementById("greenGrid").style.content= "url(pics/battlebackground.png)"
+
+document.getElementById("textBox").style.width="79%";
+document.getElementById("textBox").style.height="32%";
+document.getElementById("textBox").style.position="absolute";
+document.getElementById("textBox").style.left="10%"
+document.getElementById("textBox").style.top="52%"
+document.getElementById("textBox").style.zIndex="1"
+document.getElementById("textBox").style.content= "url(pics/textBox.png)"
