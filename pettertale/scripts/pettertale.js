ae211b49812ad6e9ca7f266b4fa6497f7398e889
AtillaBosma <atillabosma@hotmail.com>
2015-11-01 22:03:34 +0100

    test

--- pettertale/scripts/pettertale.js
+++ pettertale/scripts/pettertale.js
@@ -1,6 +1,6 @@
 document.getElementById("githubButton").style.width="619px";
 document.getElementById("githubButton").style.height="245px";
-document.getElementById("githubButton").style.bottom="10px";
-document.getElementById("githubButton").style.right="10px";
+document.getElementById("githubButton").style.bottom="100px";
+document.getElementById("githubButton").style.right="100px";
 document.getElementById("githubButton").style.position="fixed";
 document.getElementById("githubButton").style.content= "url(pics/battlebackground.png)"
