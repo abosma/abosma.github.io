b2f81f26b43e6cd8151cd91b4dce61bc2c7b1597
AtillaBosma <atillabosma@hotmail.com>
2015-11-01 22:00:54 +0100

    good SHIT

---
+++ pettertale/scripts/pettertale.js
@@ -0,0 +1,6 @@
+document.getElementById("githubButton").style.width="619px";
+document.getElementById("githubButton").style.height="245px";
+document.getElementById("githubButton").style.bottom="10px";
+document.getElementById("githubButton").style.right="10px";
+document.getElementById("githubButton").style.position="fixed";
+document.getElementById("githubButton").style.content= "url(pics/battlebackground.png)"
