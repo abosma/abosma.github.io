b838191037aff3425c5ef2bbe1b3043adcf30954
AtillaBosma <atil_duty@hotmail.com>
2015-11-02 12:51:04 +0100

    tried a different fix

--- pettertale/scripts/pettertale.js
+++ pettertale/scripts/pettertale.js
@@ -1,7 +1,8 @@
-if(window.screen.availWidth >= 1440){
 document.getElementById("dog").style.position="absolute";
-document.getElementById("dog").style.left="36.3%"
-document.getElementById("dog").style.top="7%"
+document.getElementById("dog").style.width="14%"
+document.getElementById("dog").style.height="48%"
+document.getElementById("dog").style.left="35.7%"
+document.getElementById("dog").style.top="3%"
 document.getElementById("dog").style.zIndex="2"
 document.getElementById("dog").style.content= "url(pics/lesserdog.png)"
 
@@ -19,29 +20,3 @@ document.getElementById("textBox").style.left="10%"
 document.getElementById("textBox").style.top="52%"
 document.getElementById("textBox").style.zIndex="1"
 document.getElementById("textBox").style.content= "url(pics/textbox.png)"
-}
-
-if(window.screen.availWidth < 1440){
-document.getElementById("dog").style.position="absolute";
-document.getElementById("dog").style.left="37.6%"
-document.getElementById("dog").style.width="162px"
-document.getElementById("dog").style.height="309px"
-document.getElementById("dog").style.top="3.5%"
-document.getElementById("dog").style.zIndex="2"
-document.getElementById("dog").style.content= "url(pics/lesserdog.png)"
-
-document.getElementById("greenGrid").style.width="85%";
-document.getElementById("greenGrid").style.height="50%";
-document.getElementById("greenGrid").style.position="absolute";
-document.getElementById("greenGrid").style.left="7%"
-document.getElementById("greenGrid").style.zIndex="0"
-document.getElementById("greenGrid").style.content= "url(pics/battlebackground.png)"
-
-document.getElementById("textBox").style.width="79%";
-document.getElementById("textBox").style.height="32%";
-document.getElementById("textBox").style.position="absolute";
-document.getElementById("textBox").style.left="10%"
-document.getElementById("textBox").style.top="52%"
-document.getElementById("textBox").style.zIndex="1"
-document.getElementById("textBox").style.content= "url(pics/textbox.png)"
-}
