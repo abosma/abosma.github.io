e7e38c810519e2973aa48cb6f9c60cf1b5f1d8a2
AtillaBosma <atillabosma@hotmail.com>
2016-04-30 13:57:16 +0200

    slow but steady

--- dxhacking/js/createIcons.js
+++ dxhacking/js/createIcons.js
@@ -6,12 +6,34 @@
     return [randomX, randomY];
 }
 
+function showControls() {
+    var x = event.clientX;
+    var y = event.clientY;
+    if (!document.getElementById("hackingControls")) {
+        var hackingControls = document.createElement('IMG');
+        hackingControls.setAttribute("style", "position:absolute;");
+        hackingControls.setAttribute("src", "images/hackingControls.png");
+        hackingControls.setAttribute("id", "hackingControls");
+        document.body.appendChild(hackingControls);
+        hackingControls.style.top = y + 'px';
+        hackingControls.style.left = (x + 20) + 'px';
+    } else {
+        document.getElementById("hackingControls").style.top = y + 'px';
+        document.getElementById("hackingControls").style.left = (x + 20) + 'px';
+    }
+}
+
+function removeControls() {
+    $("#hackingControls").remove();
+}
+
 
 window.onload = function () {
     var allyNode = document.createElement('IMG');
     allyNode.setAttribute("style", "position:absolute;");
     allyNode.setAttribute("src", "images/allyNode2.png");
     allyNode.setAttribute("id", "allyNode");
+    allyNode.setAttribute("onclick", "showControls();");
     document.body.appendChild(allyNode);
     var xy = getRandomPosition(allyNode);
     allyNode.style.top = xy[1] + 'px';
@@ -21,6 +43,7 @@ window.onload = function () {
     enemyNode.setAttribute("style", "position:absolute;");
     enemyNode.setAttribute("src", "images/enemyNode2.png");
     enemyNode.setAttribute("id", "enemyNode");
+    enemyNode.setAttribute("onclick", "showControls();");
     document.body.appendChild(enemyNode);
     var xy = getRandomPosition(enemyNode);
     enemyNode.style.top = xy[1] + 'px';
@@ -32,6 +55,7 @@ window.onload = function () {
         file[i].setAttribute("style", "position:absolute;");
         file[i].setAttribute("src", "images/fileUncap1.png");
         file[i].setAttribute("id", "file" + i);
+        file[i].setAttribute("onclick", "showControls();");
         document.body.appendChild(file[i]);
         var xy = getRandomPosition(file[i]);
         file[i].style.top = xy[1] + 'px';
@@ -42,5 +66,6 @@ window.onload = function () {
     canvas.setAttribute("id", "lineCanvas");
     canvas.setAttribute("width", $(window).width());
     canvas.setAttribute("height", $(window).height());
+    canvas.setAttribute("onclick", "removeControls();");
     document.body.appendChild(canvas);
 }
