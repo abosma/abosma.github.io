d50300cbcd8a84a1df18e2fb87461c2a325b6983
AtillaBosma <atillabosma@hotmail.com>
2016-05-03 02:00:43 +0200

    seperating buttons

--- dxhacking/js/createIcons.js
+++ dxhacking/js/createIcons.js
@@ -11,20 +11,85 @@ function showControls() {
     var y = event.clientY;
     if (!document.getElementById("hackingControls")) {
         var hackingControls = document.createElement('IMG');
+        var hackingBtnCapture = document.createElement("INPUT");
+        var hackingBtnDefend = document.createElement("INPUT");
+        var hackingBtnNuke = document.createElement("INPUT");
+        var hackingBtnStop = document.createElement("INPUT");
+
         hackingControls.setAttribute("style", "position:absolute;");
         hackingControls.setAttribute("src", "images/hackingControls.png");
         hackingControls.setAttribute("id", "hackingControls");
+
+        hackingBtnCapture.setAttribute("type", "image");
+        hackingBtnCapture.setAttribute("src", "images/hackingBtnCapture.png");
+        hackingBtnCapture.setAttribute("style", "position:absolute;");
+        hackingBtnCapture.setAttribute("id", "hackingBtnCapture");
+        hackingBtnCapture.setAttribute("onmouseover", "hackingBtnCapture");
+
+        hackingBtnDefend.setAttribute("type", "image");
+        hackingBtnDefend.setAttribute("src", "images/hackingBtnDefend.png");
+        hackingBtnDefend.setAttribute("style", "position:absolute;");
+        hackingBtnDefend.setAttribute("id", "hackingBtnDefend");
+        hackingBtnDefend.setAttribute("onmouseover", "hackingBtnCapture");
+
+        hackingBtnNuke.setAttribute("type", "image");
+        hackingBtnNuke.setAttribute("src", "images/hackingBtnNuke.png");
+        hackingBtnNuke.setAttribute("style", "position:absolute;");
+        hackingBtnNuke.setAttribute("id", "hackingBtnNuke");
+        hackingBtnNuke.setAttribute("onmouseover", "hackingBtnCapture");
+
+        hackingBtnStop.setAttribute("type", "image");
+        hackingBtnStop.setAttribute("src", "images/hackingBtnStop.png");
+        hackingBtnStop.setAttribute("style", "position:absolute;");
+        hackingBtnStop.setAttribute("id", "hackingBtnStop");
+        hackingBtnStop.setAttribute("onmouseover", "hackingBtnCapture");
+
+        document.body.appendChild(hackingBtnCapture);
+        document.body.appendChild(hackingBtnDefend);
+        document.body.appendChild(hackingBtnNuke);
+        document.body.appendChild(hackingBtnStop);
+
         document.body.appendChild(hackingControls);
-        hackingControls.style.top = y + 'px';
-        hackingControls.style.left = (x + 20) + 'px';
+
+        hackingControls.style.top = (y - 80) + 'px';
+        hackingControls.style.left = (x + 40) + 'px';
+
+        hackingBtnCapture.style.top = (y - 80) + 'px';
+        hackingBtnCapture.style.left = (x + 40) + 'px';
+
+        hackingBtnDefend.style.top = (y - 80) + 'px';
+        hackingBtnDefend.style.left = (x + 40) + 'px';
+
+        hackingBtnNuke.style.top = (y - 80) + 'px';
+        hackingBtnNuke.style.left = (x + 40) + 'px';
+
+        hackingBtnStop.style.top = (y - 80) + 'px';
+        hackingBtnStop.style.left = (x + 40) + 'px';
+
     } else {
-        document.getElementById("hackingControls").style.top = y + 'px';
+        document.getElementById("hackingControls").style.top = (y - 80) + 'px';
         document.getElementById("hackingControls").style.left = (x + 20) + 'px';
+
+        document.getElementById("hackingBtnCapture").style.top = (y - 80) + 'px';
+        document.getElementById("hackingBtnCapture").style.left = (x + 20) + 'px';
+
+        document.getElementById("hackingBtnDefend").style.top = (y - 80) + 'px';
+        document.getElementById("hackingBtnDefend").style.left = (x + 20) + 'px';
+
+        document.getElementById("hackingBtnNuke").style.top = (y - 80) + 'px';
+        document.getElementById("hackingBtnNuke").style.left = (x + 20) + 'px';
+
+        document.getElementById("hackingBtnStop").style.top = (y - 80) + 'px';
+        document.getElementById("hackingBtnStop").style.left = (x + 20) + 'px';
     }
 }
 
 function removeControls() {
     $("#hackingControls").remove();
+    $("#hackingBtnCapture").remove();
+    $("#hackingBtnDefend").remove();
+    $("#hackingBtnNuke").remove();
+    $("#hackingBtnStop").remove();
 }
 
 
