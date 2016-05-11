6b486df0f8ef0ba32eb25e4217893a22d788eeea
AtillaBosma <atillabosma@hotmail.com>
2016-05-11 18:06:33 +0200

    fire emblem test

---
+++ firemblem/scripts/statRandomizer.js
@@ -0,0 +1,9 @@
+﻿function init() {
+    document.getElementById("strText").textContent = Math.floor(Math.random() * 6) + 1
+    document.getElementById("defText").textContent = Math.floor(Math.random() * 6) + 1
+    document.getElementById("magText").textContent = Math.floor(Math.random() * 6) + 1
+    document.getElementById("skillText").textContent = Math.floor(Math.random() * 6) + 1
+    document.getElementById("spdText").textContent = Math.floor(Math.random() * 6) + 1
+    document.getElementById("lckText").textContent = Math.floor(Math.random() * 6) + 1
+    document.getElementById("resText").textContent = Math.floor(Math.random() * 6) + 1
+}
