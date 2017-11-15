6ce0d741cac79ab1d794409cff6ad2e48bfeeb88
abosma <atillabosma@hotmail.com>
2017-11-15 20:11:05 +0100

    asdf

--- tillycup/scripts/GetCurrentBracket.js
+++ tillycup/scripts/GetCurrentBracket.js
@@ -1,2 +1,16 @@
 var apiKey = 'RUsKQJZDAwUmKLHpKeSZ30tg2Z4V2xBEYhrFPEx6';
 
+$(document).ready(function() {
+    $.ajax({
+      url: 'https://api.challonge.com/v1/tournaments.json',
+      type: 'GET',
+      dataType: 'json',
+      success: function() { alert('hello!'); },
+      error: function() { alert('boo!'); },
+      beforeSend: setHeader
+    });
+  });
+
+  function setHeader(xhr) {
+    xhr.setRequestHeader('api_key', apiKey);
+  }
