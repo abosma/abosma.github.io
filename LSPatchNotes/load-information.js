d323c93f3e7e5f639b842949e6f936da77152784
abosma <atillabosma@hotmail.com>
2019-11-21 22:09:15 +0100

    Improvements Website

--- LSPatchNotes/load-information.js
+++ LSPatchNotes/load-information.js
@@ -11,21 +11,18 @@ fetch("https://api.github.com/repos/LeagueSandbox/GameServer/commits?per_page=5"
                 CreateAndFillElements(commitJson, "gameServerCommitList");
             }
         )
-})
 
-fetch("https://api.github.com/repos/LeagueSandbox/LeagueSandbox-Default/commits?per_page=5", {cache: "default"})
-    .then(function(response)
-    {
-        return response.json();
-    })
-    .then(function(commitHistory)
-    {
-        commitHistory.forEach(
-            function(commitJson)
-            {
-                CreateAndFillElements(commitJson, "defaultContentCommitList");
-            }
-        )
+        var commitList = document.getElementById("gameServerCommitList");
+        
+        var commitListButton = document.createElement("A");
+        commitListButton.id = "patchnotesbutton";
+        commitListButton.className = "btn btn-lg btn-block";
+        commitListButton.href = "https://github.com/LeagueSandbox/GameServer/commits/indev"
+        commitListButton.role = "button";
+        commitListButton.textContent = "Full patch-notes list";
+        commitListButton.style = "color: #d2eae5"
+
+        commitList.appendChild(commitListButton);
 })
 
 function CreateAndFillElements(inputJson, listClassName)
@@ -35,33 +32,41 @@ function CreateAndFillElements(inputJson, listClassName)
     var commitMessage = inputJson.commit.message;
     var commitAuthor = inputJson.author.login;
     var commitAuthorIcon = inputJson.author.avatar_url;
-    var commitAuthorLink = inputJson.author.html_url;
 
     var commitList = document.getElementById(listClassName);
-    var commitNode = document.createElement("LI");
-    var commitAuthorImage = document.createElement("IMG");
-    var commitAuthorName = document.createElement("H3");
-    var commitAuthorNameText = document.createTextNode(commitAuthor);
-    var commitDateElement = document.createElement("H2");
-    var commitDateElementText = document.createTextNode(commitDate);
-    var commitListMessage = document.createElement("P");
-    var commitListMessageText = document.createTextNode(commitMessage);
 
-    commitAuthorImage.src = commitAuthorIcon;
-    commitAuthorName.appendChild(commitAuthorNameText);
-    commitAuthorName.href = commitAuthorLink;
-    commitListMessage.appendChild(commitListMessageText);
-    commitDateElement.appendChild(commitDateElementText);
+    var commitMedia = document.createElement("LI");
+    commitMedia.className = "media rounded p-3 my-3";
 
-    commitNode.appendChild(commitAuthorImage);
-    commitNode.appendChild(commitAuthorName);
-    commitNode.appendChild(commitListMessage);
-    commitNode.appendChild(commitDateElement);
+    var commitMediaBody = document.createElement("DIV")
+    commitMediaBody.className = "media-body";
 
-    commitNode.onclick = function()
+    var commitMediaImage = document.createElement("IMG");
+    commitMediaImage.src = commitAuthorIcon;
+    commitMediaImage.className = "align-self-center mr-3 rounded-circle";
+    commitMediaImage.style = "width:64px"
+
+    var commitMediaAuthor = document.createElement("H3");
+    commitMediaAuthor.textContent = commitAuthor;
+
+    var commitMediaDate = document.createElement("H6");
+    commitMediaDate.textContent = commitDate;
+
+    var commitMediaMessage = document.createElement("P");
+    commitMediaMessage.textContent = commitMessage;
+
+    commitMediaAuthor.appendChild(commitMediaDate);
+
+    commitMediaBody.appendChild(commitMediaAuthor);
+    commitMediaBody.appendChild(commitMediaMessage);
+
+    commitMedia.appendChild(commitMediaImage);
+    commitMedia.appendChild(commitMediaBody);
+
+    commitMedia.onclick = function()
     {
         window.location.href = commitUrl;
     }
 
-    commitList.appendChild(commitNode);
+    commitList.appendChild(commitMedia);
 }
